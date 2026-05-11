d73631d6e05d1ad94048145165179b75ff44cfea
Usman Younas <usmanyounasbiba@gmail.com>
2026-05-11 10:49:08 +0500

    add

--- src/components/Sufyan.jsx
+++ src/components/Sufyan.jsx
@@ -4,13 +4,10 @@ const Sufyan = () => {
   return (
     <div>
       <h1> Add by sufyan</h1>
-<<<<<<< Updated upstream
        <button type="button" class="btn btn-danger">Submit</button>
-=======
       <div class="input-group flex-nowrap">
   <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"/>
 </div>
->>>>>>> Stashed changes
     </div>
   )
 }
